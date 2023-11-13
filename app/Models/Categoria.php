32c860ba14b4035c0e657b69eb5abbc58fe1432a
sayviorc <60513103+sayviorc@users.noreply.github.com>
2023-11-12 18:01:29 -0600

    Initial commit

---
+++ app/Models/Categoria.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    use HasFactory;
+
+    protected $table= 'Categorias';
+    protected $primaryKey = 'Id_categoria';
+    public $timestamps = false;
+
+    protected $fillable = ['Categoria','Descripcion','Estatus'];
+}
